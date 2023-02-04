33a93758ea8d2c1662662d647582b0fc07b38c82
Vladimir Bubenko <bawmc@mail.ru>
2023-02-04 18:16:39 +0700

    done task 21

--- Program.cs
+++ Program.cs
@@ -14,13 +14,36 @@
 // Console.WriteLine(Poly(number));
 
 // Вариант 2:
-void Poly(int number)
+// void Poly(int number)
+// {
+//     int newnumber = (number%10*10000 + number/10%10*1000+number/100%10*100+number/1000%10*10+number/10000);
+//     if (number == newnumber) Console.WriteLine("Yes, it is palindrome");
+//     else Console.WriteLine("No, it is random number");
+// }
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Poly(number);
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double result(double xA, double yA, double zA, double xB, double yB, double zB)
 {
-    int newnumber = (number%10*10000 + number/10%10*1000+number/100%10*100+number/1000%10*10+number/10000);
-    if (number == newnumber) Console.WriteLine("Yes, it is palindrome");
-    else Console.WriteLine("No, it is random number");
+    double lenth = Math.Sqrt(Math.Pow((xB-xA),2) + Math.Pow((yB-yA),2) + Math.Pow((zB-zA),2));
+    return lenth;
 }
-Console.WriteLine("Input number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Poly(number);
+Console.Write("Input xA ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yA ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input zA ");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input xB: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yB: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input zB ");
+double zB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Round(result(xA,yA,zA,xB,yB,zB),2));
+
+
 
